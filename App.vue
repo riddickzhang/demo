b6e9c45a59f094d67cce039ad6830745f418d7b0
riddick.zhang <riddick.zhang@riddickzhangdeMacBook-Pro.local>
2022-07-20 17:48:04 +0800

    123

--- App.vue
+++ App.vue
@@ -6,39 +6,34 @@
         <s-col>2 of 3</s-col>
         <s-col>3 of 3</s-col>
       </s-row>
-      <s-row>
-        <s-input v-model="data1" placeholder="请输入内容"></s-input
-      ></s-row>
+      <s-row> <s-input v-model="data1" placeholder="请输入内容"></s-input></s-row>
 
-      <s-pagination
-        v-model="current"
-        :total="pageTotal"
-        :default-page-size="pageSize"
-      ></s-pagination>
+      <s-pagination v-model="current" :total="pageTotal" :default-page-size="pageSize"></s-pagination>
+      <span>123321</span>
     </s-container>
   </div>
 </template>
 
 <script>
   export default {
-    name: "App",
+    name: 'App',
     components: {},
-    data: ()=>({
-      data1:0,
-      pageTotal:20,
+    data: () => ({
+      data1: 0,
+      pageTotal: 20,
       current: 1,
-      pageSize:10,
-    })
-  };
+      pageSize: 10,
+    }),
+  }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
